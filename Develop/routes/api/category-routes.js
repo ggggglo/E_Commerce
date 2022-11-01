7e37c9f3146e2849383b90cec70f99ee3c023f43
Gloriana Gonzalez <glogonzalez@Glorianas-MacBook-Pro.local>
2022-11-01 15:35:47 -0500

    created route to create a new category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -45,7 +45,14 @@ router.get('/:id', (req, res) => {
 
 // Create new category
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+      category_name: req.body.category_name
+    })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // Update category by its id value
