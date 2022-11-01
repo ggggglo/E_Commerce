1a4aedae70342139c015d99fe21389a78b9283fb
Gloriana Gonzalez <glogonzalez@Glorianas-MacBook-Pro.local>
2022-11-01 15:34:15 -0500

    created route to find all categories and its associated products

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,24 +3,39 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// Find all categories and its associated products
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+      include: [{
+        model: Product
+      }]
+    })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
+
+
+// Find one category by its id value and its associated products
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
+// Create new category
 router.post('/', (req, res) => {
   // create a new category
 });
 
+// Update category by its id value
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
+// Delete category by its id value
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
 });
