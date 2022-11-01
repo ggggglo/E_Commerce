63089fb5d024d0faa29d63a6121788eafb859d11
Gloriana Gonzalez <glogonzalez@Glorianas-MacBook-Pro.local>
2022-11-01 15:46:27 -0500

    create a new product

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -25,25 +25,38 @@ router.get('/', (req, res) => {
 });
 
 // Fin one product and its associated category and tag data
-// get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [{
+          model: Category
+        },
+        {
+          model: Tag,
+          as: 'productTag'
+        }
+      ]
+    })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({
+          message: 'No product found with this id!'
+        });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
-// create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+// Create a new product
+Product.create(req.body)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
@@ -53,7 +66,6 @@ router.post('/', (req, res) => {
         });
         return ProductTag.bulkCreate(productTagIdArr);
       }
-      // if no product tags, just respond
       res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
@@ -61,7 +73,6 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-});
 
 // update product
 router.put('/:id', (req, res) => {
